aabb55c3072d4ed0c84c7e1c4e47242365a35880
Imran Hossain <your-email@example.com>
2025-06-16 07:15:18 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust depending on your file types and project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
